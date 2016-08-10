cc405cc70537bee0c852d1d3c21dfa5deccbd7e1
xuqiuyang <1556147829@qq.com>
2016-08-10 10:27:34 +0800

    自动化部署工具

--- package.sh
+++ package.sh
@@ -4,6 +4,7 @@
 #export dir
 base_dir=$(cd `dirname $0`; pwd)
 
+#*****************************************************************************************
 cd $base_dir
 
 svn_file="./svn.txt"
@@ -21,11 +22,11 @@ else
         begin_version=0
 fi
 
-end_version=`svn info $svn_path  |grep Revision: |awk   '{print $2}'`
+end_version=`svn info $svn_path  | grep Revision: | awk   '{print $2}'`
 
 if [ $begin_version = $end_version ]
 then
-	echo "svn version is lastest, nothing to do!"
+	echo "[warn] svn version is lastest, nothing to do!"
 	exit 0
 fi
 
@@ -45,18 +46,12 @@ delete_log="svndel.m"
 #export add,commit file
 for i in `svn diff -r $begin_version:$end_version   --summarize $svn_path | grep '^[AM]' | sed 's:^[AM]\s*::g'`;do
 	
-	#echo $i;
-	
 	str1=`echo $i | sed 's#\.#!1!#g' | sed 's#\/#!2!#g' | sed 's#\:#!3!#g'`;
 	str2=`echo $svn_path | sed 's#\.#!1!#g' | sed 's#\/#!2!#g' | sed 's#\:#!3!#g'`;
 	
 	str3=`echo $str1 | sed 's:'$str2'::g'`;	
 	
-	#echo $str3;
-
 	str4=`echo $str3 | sed 's/!1!/\./g' | sed 's/!2!/\//g' | sed 's/!3!/\:/g'`
-
-	#echo $str4;
 
 	all_path=`echo $str4`;
 
@@ -83,40 +78,58 @@ for i in `svn diff -r $begin_version:$end_version   --summarize $svn_path | grep
 
         all_path=`echo $str4`;
 
-
-        #all_path=`echo $i`;
-
 	echo $all_path >> ${version}${delete_log}
 
 done
 
-#write svn log
+#*****************************************************************************************
+#delete ignore config file
+cd $base_dir
+for i in `cat ./ignore`;do
+	
+	if [ -f ${version}${i} ];then
+		echo "[info] delete igone file: "${version}${i}
+		rm -rf ${version}${i}
+	fi;
+	
+done
+
+#******************************************************************************************
 cd ${version}
 time=$(date +%Y%m%d%H%M%S)
 svn diff -r $begin_version:$end_version   --summarize $svn_path > ${base_path}${time}.txt
 
 #dabao
 #zip -r ${base_path}${begin_version}'-'${end_version}".zip" "./"
+if [ `ls ./`=''  ];then
+	echo "[warn] no file need update"
+
+	#rollback
+	cd $base_dir
+	rm -rf $base_path
+		
+	exit 0
+fi;
 
 echo "\npackage....\n"
-zip -r ${base_path}"version.zip" "./"
+zip 	-r ${base_path}"version.zip" "./"
 
 cp  ${base_path}"version.zip" ../../lastest-version.zip
 
-echo "\nupdate version txt....\n"
+echo "\n[info] make zip file finished!\n"
 
+#******************************************************************************************
 cd $base_dir
 
 echo $end_version > $version_file
 
+echo "\n[info] update version file finished!\n"
+
+echo '\n\n[info] Package Complete!\n'
 
 
 
-
-echo '\n\nPackage Complete!\n'
-
-
-
+#ansbile deploy
 ansible-playbook deploy.yml
 
 
